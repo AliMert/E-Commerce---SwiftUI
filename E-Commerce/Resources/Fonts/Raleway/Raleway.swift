06b506e393c2017fa4fe61c434a4d737e7fe64a1
Ali Mert Özhayta <mozhayta@hotmail.com>
2022-05-28 17:39:32 +0300

    add resources and fonts

---
+++ E-Commerce/Resources/Fonts/Raleway/Raleway.swift
@@ -0,0 +1,12 @@
+//
+//  Raleway.swift
+//  E-Commerce
+//
+//  Created by Ali Mert Özhayta on 28.05.2022.
+//
+
+enum Raleway {
+    static let regular = "Raleway-Regular"
+    static let bold = "Raleway-Bold"
+    static let semibold = "Raleway-SemiBold"
+}
