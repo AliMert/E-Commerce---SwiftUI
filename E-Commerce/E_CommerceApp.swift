6f403bab4535f0d3c455a90e2d70f8054259aaea
Ali Mert Özhayta <mozhayta@hotmail.com>
2022-05-28 17:20:57 +0300

    Initial Commit

---
+++ E-Commerce/E_CommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceApp.swift
+//  E-Commerce
+//
+//  Created by Ali Mert Özhayta on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
